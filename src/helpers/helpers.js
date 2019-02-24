e09e2218c5db28522b90de57548a58349e5dde0e
osycon <fredrichawk@gmail.com>
2019-02-24 16:30:51 +0100

    feat: Added api call for a book.

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -6,20 +6,21 @@ dotenv.config()
 const apiKey = process.env.GOOGLE_BOOKS_API_KEY
 const baseURL = 'https://www.googleapis.com/books/v1/volumes'
 
-export const getData = async query => {
+export const getBooks = async query => {
   const url = `${baseURL}?q=${query}&key=${apiKey}`
   const { data } = await axios({
     method: 'get',
     url,
   })
-  // .then(res => {
-  //   return { message: 'Success', res }
-  // })
-  // .catch(err => {
-  //   return { message: 'Error', err }
-  // })
 
   return data
 }
 
-export const more = () => 'win'
+export const getBook = async id => {
+  const url = `${baseURL}/${id}`
+  const { data } = await axios({
+    method: 'get',
+    url,
+  })
+  return data
+}
