b2d42e1ecc8349ee2935cd638f35c9a5ad615c8a
osycon <fredrichawk@gmail.com>
2019-02-24 17:39:09 +0100

    feat: Added helper for truncating description.

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -24,3 +24,10 @@ export const getBook = async id => {
   })
   return data
 }
+
+export const truncate = (str, length = 100, ending = '...') => {
+  if (str.length > length) {
+    return str.substring(0, length - ending.length) + ending
+  }
+  return str
+}
