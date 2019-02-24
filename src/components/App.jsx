78ff8a47cd01b11c708875646c62a435e33c4aba
osycon <fredrichawk@gmail.com>
2019-02-24 19:28:47 +0100

    feat: Added 404 page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import { Router } from '@reach/router'
 import React from 'react'
 import BookDetails from './BookDetails'
 import Home from './Home'
+import NotFound from './NotFound'
 
 const App = () => {
   return (
     <div>
       <Router>
+        <NotFound default />
         <Home path="/" />
-        <BookDetails path=":details" />
+        <BookDetails path="/book/:details" />
       </Router>
     </div>
   )
