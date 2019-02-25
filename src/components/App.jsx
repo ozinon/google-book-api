df505de908f5e2696328f50e597a214a2cfa17a0
osycon <fredrichawk@gmail.com>
2019-02-25 19:14:45 +0100

    feat: Added footer and styling for it.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import { Router } from '@reach/router'
 import React from 'react'
 import BookDetails from './BookDetails'
+import Footer from './Footer'
 import Home from './Home'
 import NotFound from './NotFound'
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <NotFound default />
-        <Home path="/" />
-        <BookDetails path="/book/:details" />
-      </Router>
+      <div className="main-content">
+        <Router>
+          <NotFound default />
+          <Home path="/" />
+          <BookDetails path="/book/:details" />
+        </Router>
+      </div>
+      <Footer />
     </div>
   )
 }
